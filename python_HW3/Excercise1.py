10e0f4bbb0c8d3e25a8867dd2ef042df67a8344d
Madhavinamballa <madhavi@Madhavinamballas-MacBook-Pro.local>
2020-09-14 15:05:41 -0700

    added Ecercise1 from HW3

---
+++ python_HW3/Excercise1.py
@@ -0,0 +1,18 @@
+count = 0
+total = 0
+avg = 0
+choice = True
+while choice:
+  number = input ('Enter a number: ')
+  try:
+     number = float(number)
+     count = count + 1 
+     total = total + number
+     avg = total / count
+  except:
+    if number.lower()=='done':
+      break
+    else:
+      print ('Invalid Input')
+      continue
+print (f'total:{total} \ncount: {count}\naverage:{avg}')
