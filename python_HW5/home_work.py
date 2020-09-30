d4e96b8c0157ec8e1bcf7863d24a1dba2384bfdd
Madhavinamballa <madhavi@Madhavinamballas-MacBook-Pro.local>
2020-09-29 19:36:47 -0700

    addedd working cop

--- python_HW5/home_work.py
+++ python_HW5/home_work.py
@@ -1,26 +1,26 @@
 from collections import Counter
 fname=input("Enter the file name: ")
 count=0
-email=[]
-try:
-    fhand=open(fname)
-    for line in fhand:
+values=[]
+fhand=open(fname)
+for line in fhand:
 #         line=line.strip()
-        if not line.startswith('From'):
-            continue
-        count=count+1
-        words=line.split()
-        email.append[words[1]]
-        print(words[1])
-    print('there were ',count,'lines in the file with From as the first word')
-except:
-    print('file cannot be opened: ',fname)
-    exit()
-list_dict=dict(Counter(email))
+    if not line.startswith('From'):
+        continue
+    count=count+1
+    words=line.split()
+    values.append[words[1]]
+list_dict=dict(Counter(values))
 print(list_dict)
-for key,value in list_dict.items():
-    print(key,': ',value)
-sort_orders = sorted(list_dict.items(), key=lambda x: x[1], reverse=True)
+def keywithmaxval(d):
+     v=list(d.values())
+     k=list(d.keys())
+     return [k[v.index(max(v))],max(v)]
+print(keywithmaxval(list_dict)[0],keywithmaxval(list_dict)[1])
 
-for i in sort_orders:
-    print(i[0], i[1])
+def keywithminval(d):
+     v=list(d.values())
+     k=list(d.keys())
+     return [k[v.index(min(v))],min(v)]
+print(keywithmaxval(list_dict)[0],keywithmaxval(list_dict)[1])
+print(keywithminval(list_dict)[0],keywithminval(list_dict)[1])
